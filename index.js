a91b4e6b81b419d65f6f5e8921e44efd92b58189
Sadaf Farahmandifard <sadaffarahmandi@yahoo.com>
2019-06-23 04:48:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function sayHiToGrandma(string){
-  if (string.toLowerCase){
-    console.log("I can\'t hear you!")
-  }
-  if (string.toUpperCase) {
-    console.log("YES INDEED!")
-  }
-  if ("string.iLoveYouGrandma") {
-    console.log("I love you, too.")
-  }
+function sayHiToGrandma("string") {
+  return ("I love you, too.")
 }
