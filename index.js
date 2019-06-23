5538c7922b3d99a599914d6583847abe2bd891e8
Sadaf Farahmandifard <sadaffarahmandi@yahoo.com>
2019-06-23 05:41:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase())
-  return ("I can't hear you!")
+  return ("I can't hear you!") ;
 
 }
 function sayHiToGrandma(string) {
   if (string.toUpperCase())
-  return ("YES INDEED!")
+  return ("YES INDEED!") ;
 }
