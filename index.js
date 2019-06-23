aa8855cdbe5c63363b83b99aa3338ab91e22f029
Sadaf Farahmandifard <sadaffarahmandi@yahoo.com>
2019-06-23 05:42:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 function whisper(string){
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 function logShout(string) {
-  console.log('HELLO')
+  console.log('HELLO');
 }
 function logWhisper(string) {
-  console.log('hello')
+  console.log('hello');
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase())
